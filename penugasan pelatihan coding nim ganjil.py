97cbce586d9b2405c59285e13b66ac96aabd55df
025_Achmad_Chusnul_Yakin <143864302+AchmadChusnulYakin@users.noreply.github.com>
2023-09-10 15:33:13 +0700

    Add files via upload

---
+++ penugasan pelatihan coding nim ganjil.py
@@ -0,0 +1,34 @@
+# Mengkonfersi Celcius dalam Fahrenheit
+
+# Suhu dalam Celcius
+celcius = 10
+
+# Suhu dalam Fahrenheit
+fahrenheit = 9* celcius/5+32
+
+# Hasil konfersi suhu dalam Celcius ke Fahrenheit
+print("Hasil dari konversi 10 Celcius ke Fahrenheit adalah", fahrenheit)
+
+
+# Mengkonfersi Suhu dalam Fahrenheit ke Celcius
+
+# Suhu dalam Fahrenheit
+Fahrenheit = 68
+
+# Suhu dalam Celcius
+celcius = (5/9)*(Fahrenheit-32)
+
+# Hasil konfersi Suhu dalam Fahrenheit ke Celcius
+print ("Hasil dari konfersi 68 Fahrenheit ke Celcius adalah", celcius)
+
+
+# Mengkonfersi Suhu dalam Kelvin ke Celcius
+
+# Suhu dalam Kelvin
+Kelvin = 300 
+
+# Suhu dalam Celcius
+celcius = 300  - 273.15
+
+# Hasil konfersi Suhu dalam Fahrenheit ke Celcius
+print ("Hasil dari konfersi 300 Kelvin ke Celcius adalah", celcius)
